e228347c80ded934f827a29bfe27e6a27579653d
jiangtj <116749895@qq.com>
2020-06-29 16:57:51 +0800

    .

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ If you use [next](https://github.com/theme-next/hexo-theme-next) or [cake](https
 yarn add @jiangtj/hexo-next-pwa
 ```
 
-Others, you have to install hexo 4.2.1+ or master.
+Others, you have to install hexo 5.0 or master.
 
 ## Configure
 
--- test/index.js
+++ test/index.js
@@ -7,9 +7,15 @@ const hexo = new Hexo(__dirname, { silent: true });
 const { load } = require('js-yaml');
 const { readFileSync, writeFileSync } = require('fs');
 const { resolve } = require('path');
+const { generateSW } = require('workbox-build');
 
 describe('main', () => {
   const config = load(readFileSync(resolve(__dirname, '../default.yaml'), 'utf8')).pwa;
+
+  generateSW(Object.assign({
+    'mode': 'dev'
+  }, config.serviceWorker.options));
+
   const template = require('../lib/generate-sw-string')(config.serviceWorker);
   const result = template.data();
   console.log(result);
