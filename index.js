cd2688c9f6515dabbd1112fc5456f068bb1cc41d
jiangtj <116749895@qq.com>
2021-02-02 14:48:58 +0800

    ...

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const fs = require('fs');
 const { join } = require('path');
 const injector = require('hexo-extend-injector2')(hexo);
 
-const unsafe = require('js-yaml-js-types').all;
-const schema = yaml.DEFAULT_SCHEMA.extend(unsafe);
-
-
 /**
  * config
  */
-const defaultConfig = yaml.load(fs.readFileSync(join(__dirname, 'default.yaml'), 'utf8'), {schema});
+const defaultConfig = yaml.load(fs.readFileSync(join(__dirname, 'default.yaml'), 'utf8'));
 const config = mergeWith(defaultConfig.pwa, hexo.config.pwa, (objValue, srcValue) => {
   if (Array.isArray(objValue)) {
     return srcValue;
+  }
+});
+config.serviceWorker.options.runtimeCaching.forEach(element => {
+  if (element.urlPattern.startsWith('regexp:')) {
+    element.urlPattern = new RegExp(element.urlPattern.substring(7));
   }
 });
 
