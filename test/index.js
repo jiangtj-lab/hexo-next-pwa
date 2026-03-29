756c8cc5b490bfefce5c07a94c03617be25e7ac8
Mr.J <116749895@qq.com>
2026-03-29 22:14:29 +0800

    refactor: 移除js-yaml-js-types依赖并更新配置
    - 删除不再需要的js-yaml-js-types相关文件和测试
    - 更新eslint配置为新的配置文件格式
    - 升级依赖版本并清理package.json
    - 调整.gitignore添加package-lock.json和pnpm-lock.yaml
    - 简化测试用例，移除过时的测试

--- test/index.js
+++ test/index.js
@@ -1,38 +1,31 @@
-'use strict';
+"use strict";
 
-require('chai').should();
+require("chai").should();
 
-const Hexo = require('hexo');
+const Hexo = require("hexo");
 const hexo = new Hexo(__dirname, { silent: true });
-const { readFileSync } = require('fs');
-const { join } = require('path');
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
-describe('main', () => {
-  it('test js-yaml-js-types', () => {
-    const yaml = require('js-yaml');
-    const schema = require('../lib/get-yaml-schema')(hexo);
-    const config = yaml.load(readFileSync(join(__dirname, 'js-yaml-js-types.yaml'), 'utf8'), { schema });
-    config.should.eql({ urlPattern: /\.(?:js|css)$/ });
-  });
-
-  it('test default config', () => {
-    const config = require('../lib/get-default-config')(hexo);
+describe("main", () => {
+  it("test default config", () => {
+    const config = require("../lib/get-default-config")(hexo);
     const { runtimeCaching } = config.serviceWorker.options;
     runtimeCaching[0].should.eql({
-      urlPattern: '/',
-      handler: 'NetworkFirst',
-      options: { cacheName: 'index' }
+      urlPattern: "/",
+      handler: "NetworkFirst",
+      options: { cacheName: "index" },
     });
     runtimeCaching[1].should.eql({
       urlPattern: /\.(?:js|css)$/,
-      handler: 'StaleWhileRevalidate',
-      options: { cacheName: 'js-css' }
+      handler: "StaleWhileRevalidate",
+      options: { cacheName: "js-css" },
     });
   });
 
-  it('test colorette', () => {
-    const { cyan, magenta } = require('colorette');
-    console.log('Generated: %s', magenta('C:\\Users'));
-    console.log('ServiceWorker files generated in %s', cyan('1s'));
+  it("test colorette", () => {
+    const { cyan, magenta } = require("colorette");
+    console.log("Generated: %s", magenta("C:\\Users"));
+    console.log("ServiceWorker files generated in %s", cyan("1s"));
   });
 });
